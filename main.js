b31d919801b7a043796b0e482e20929622f69da8
Kai Brabo <kaibrabo@Kais-MacBook-Pro.local>
2022-12-30 22:31:36 -0800

    add images of bulbs

--- main.js
+++ main.js
@@ -5,71 +5,15 @@ function openMenu(evt, menuName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("menu");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+        tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
     }
     document.getElementById(menuName).style.display = "block";
     evt.currentTarget.firstElementChild.className += " w3-dark-grey";
-  }
-  document.getElementById("myLink").click();
-
-
-
-
-
-// const displaySection = document.getElementById('displaySection');
-
-// // nav bar
-// const nav = document.createElement('div');
-// const aboutTab = document.createElement('div');
-// const productsTab = document.createElement('div');
-// const contactTab = document.createElement('div');
-
-// aboutTab.innerHTML = 'about';
-// productsTab.innerHTML = 'products';
-// contactTab.innerHTML = 'contact';
-// nav.setAttribute('id', 'navbar');
-// nav.append(productsTab, aboutTab, contactTab);
-
-// // display section contents
-// const sectionContents = document.createElement('div');
-// sectionContents.setAttribute('id', 'sectionContents');
-
-// // about section
-// const PETE = { name: 'Pete', title: 'Owner' };
-// const KALANI = { name: 'Kalani', title: 'Sales' };
-// const VANTE = { name: 'Vante', title: 'Sales' };
-
-// function aboutSection() {
-//     const employees = [PETE, KALANI, VANTE];
-
-//     for (let em of employees) {
-//         const component = createEmployeeSection(em);
-//         sectionContents.append(component);
-//     }
-// }
-
-// function createEmployeeSection(employee) {
-//     const component = document.createElement('div');  // <div></div>
-//     component.setAttribute('id', employee.name);    // id="name"
-
-//     const name = document.createElement('h3')
-//     const title = document.createElement('h4');
-//     name.innerHTML = employee.name;
-//     title.innerHTML = employee.title;
-    
-//     component.append(name, title);
-//     return component;
-// }
-
-// // contact section
-// function contactSection() {
-
-// }
-
-// displaySection.append(nav, sectionContents);
+}
+document.getElementById("myLink").click();
 
 
